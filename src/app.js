78ea35500bf3d6e488c12dea7e732296c85e87b4
Sai Surya Teja <saisuryateja3@gmail.com>
2024-09-18 17:28:06 +0530

    Exploring get,post,put,delete

--- src/app.js
+++ src/app.js
@@ -3,8 +3,22 @@ const express = require("express");
 const PORT = 3000;
 const app = express();
 
-app.use("/test", (req, res) => {
-  res.send("Hello from the server!");
+// app.use("/test", (req, res) => {
+//   res.send("Hello from the server!");
+// });
+
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Sai Surya Teja", lastName: "Chavatapalli" });
+});
+
+app.post("/user", (req, res) => {
+  res.send("Posted Successfully!");
+});
+app.put("/user", (req, res) => {
+  res.send("Updated Successfully!");
+});
+app.delete("/user", (req, res) => {
+  res.send("Deleted Successfully!");
 });
 
 app.listen(PORT, () => {
