6fe06f5a8f28b3934b8f3fdad754e870314bb766
Sai Surya Teja <saisuryateja3@gmail.com>
2024-09-18 18:00:47 +0530

    Dynamic params, query, routing regex routes

--- src/app.js
+++ src/app.js
@@ -3,9 +3,29 @@ const express = require("express");
 const PORT = 3000;
 const app = express();
 
-// app.use("/test", (req, res) => {
-//   res.send("Hello from the server!");
-// });
+// This is called for all HTTP methods with '/mine' route(GET,POST,PUT,DELETE)
+app.use("/mine", (req, res) => {
+  res.send("Hello from the server!");
+});
+
+// Query Params
+app.get("/test", (req, res) => {
+  res.send(req.query.testId);
+});
+
+//Dynamic Route
+app.get("/dynamic/:dynamicId", (req, res) => {
+  res.send(req.params.dynamicId);
+});
+
+// Regex Routes
+app.get(/hi/, (req, res) => {
+  res.send("This is called for routes with 'hi' in it, as it has regex");
+});
+
+app.get("/ab*c", (req, res) => {
+  res.send("This is called route staring with 'ab' and ending with 'c'");
+});
 
 app.get("/user", (req, res) => {
   res.send({ firstName: "Sai Surya Teja", lastName: "Chavatapalli" });
