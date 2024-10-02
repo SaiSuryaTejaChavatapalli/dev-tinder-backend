51c5b3c6ce8c89ef37cbf773ee2653cd5b358914
Sai Surya Teja <saisuryateja3@gmail.com>
2024-10-03 00:03:42 +0530

    PATCH, Update the user

--- src/app.js
+++ src/app.js
@@ -18,6 +18,16 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+// GET data of the user
 app.get("/user", async (req, res) => {
   const userEmail = req.body.emailId;
   try {
@@ -40,20 +50,28 @@ app.get("/user", async (req, res) => {
   }
 });
 
-app.get("/feed", async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.send(users);
-  } catch (error) {
-    res.status(400).send("Something went wrong");
-  }
-});
-
+// DELETE data of the user
 app.delete("/user", async (req, res) => {
   const userId = req.body.userId;
   try {
     const user = await User.findByIdAndDelete(userId);
     res.send("User Deleted Successfully");
+  } catch (error) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+// UPDATE data of the user
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  // It will ignore the fields the which is not part of Schema (userId)
+  try {
+    const user = await User.findByIdAndUpdate(userId, data, {
+      returnDocument: "after",
+    });
+
+    res.send("User Updated Successfully");
   } catch (error) {
     res.status(400).send("Something went wrong");
   }
